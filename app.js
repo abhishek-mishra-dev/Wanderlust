1d0090b39258b246defdca3ea15280d75b2e4d62
Abhishek Mishra <am837556@gmail.com>
2025-02-25 08:31:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -31,9 +31,13 @@ app.use(express.static(path.join(__dirname, "/public")));
 
 app.use(
   cors({
-    origin: "https://wanderlust-ehk0.onrender.com",
+    origin: "https://wanderlust-ehk0.onrender.com", 
+    credentials: true, 
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"], 
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
 
 const dbUrl = process.env.ATLASDB_URL;
 
